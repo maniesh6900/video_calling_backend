24959c68d7ff7ebbce3cbcf21d7dbe1d24b4f859
maniesh6900 <kaj882626@gmail.com>
2026-03-02 18:05:32 +0530

    removed prisma for now removed all the controllers and routes

--- src/routes/user-route.ts
+++
@@ -1,9 +0,0 @@
-import Router from "express";
-import { createToken, userLogin, userSignup } from "../controller/user-controller";
-const router = Router();
-
-router.route("/signup").post(userSignup);
-router.route("/login").post(userLogin);
-router.route("/token").post(createToken);
-
-export default router;
